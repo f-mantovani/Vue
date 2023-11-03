bc6dea3d904c281ee799ce37a3eec6201d404130
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-11-03 23:21:06 +0100

    "moving to the CLI version"

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,33 @@
+<script>
+  export default {
+    name: "Menu Item",
+    props: ['item', 'addToShoppingCart']
+  }
+</script>
+
+<template>
+  <div>
+    <img
+        :src="item.image.source"
+        :alt="item.image.alt"
+      />
+    <div>
+      <h3>{{ item.name }}</h3>
+      <p v-if="item.inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <div>
+        <label for="add-item-quantity"
+          >Quantity: {{ item.quantity }}</label
+        >
+        <input
+          v-model.number="item.quantity"
+          id="add-item-quantity"
+          type="number"
+        />
+        <button @click="addToShoppingCart(item.quantity)">
+          Add to Shopping Cart
+        </button>
+      </div>
+    </div>
+  </div>  
+</template>
